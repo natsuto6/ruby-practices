9288bafefb34619d6978151ee255bed950330269
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2022-07-27 23:11:25 +0900

    File Correction2 ls1

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,29 +3,29 @@
 
 require 'find'
 
-lists = []
-def find_lists(current_dir, lists)
-  Find.find(current_dir) do |file|
-    file_name = File.basename(file)
-    next if File.basename(current_dir) == file_name || /^\./ =~ file_name
-
-    lists << case File.ftype(file)
-             when 'directory', 'file'
-               file_name
-             end
+def find_lists(current_dir)
+  Find.find(current_dir)
+      .map { |n| File.basename(n) }
+      .filter { |n| File.basename(current_dir) != n }
+      .filter { |n| !n.start_with?('.') }
+      .map do |n|
+    case File.ftype(n)
+    when 'directory', 'file'
+      n
+    end
   end
 end
 
 def display_lists(lists, column)
-  each_column = lists.length < column ? 1 : lists.length / column
-  0.upto(each_column).each_with_index do |line, idx|
-    lists.each_slice(each_column) do |columns|
-      print columns[line].ljust(16) if columns [line]
+  line_count = (lists.length.to_f / column).ceil
+  line_count.times do |line|
+    lists.each_slice(line_count) do |columns|
+      print columns[line]&.ljust(16)
     end
-    print "\n" unless idx == each_column - 1
+    print "\n"
   end
 end
 
 current_dir = File.absolute_path('.')
-find_lists(current_dir, lists)
+lists = find_lists(current_dir)
 display_lists(lists, 3)
