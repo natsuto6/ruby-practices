30bc23010635719216d03368822a9cbfb0dd1fc8
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2022-07-31 16:17:53 +0900

    Add ls2

---
+++ 05.ls/ls2.rb
@@ -0,0 +1,30 @@
+# !/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+
+opt = OptionParser.new
+params = {}
+opt.on('-a') { |v| params[:a] = v }
+opt.parse!(ARGV)
+
+def find_lists(params)
+  if params[:a].nil?
+    Dir.glob('*')
+  else
+    Dir.glob('*', File::FNM_DOTMATCH)
+  end
+end
+
+def display_lists(lists, column)
+  line_count = (lists.length.to_f / column).ceil
+  line_count.times do |line|
+    lists.each_slice(line_count) do |columns|
+      print columns[line]&.ljust(16)
+    end
+    print "\n"
+  end
+end
+
+lists = find_lists(params)
+display_lists(lists, 3)
